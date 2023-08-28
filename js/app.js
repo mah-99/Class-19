3fd20f5b198753f2885673e8a8e24e3085c5b00d
mah-99 <mah.wd.bd@gmail.com>
2023-08-28 20:37:02 +0600

    a

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+
+
+document.getElementById('img').addEventListener('click', function(){
+    document.getElementById('img').src = 'https://c8.alamy.com/comp/D93J89/adorable-baby-boy-with-flower-rose-isolated-on-white-background-babies-D93J89.jpg'
+})
